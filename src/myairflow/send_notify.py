8736011bdc4016337f3432dbf6272cc127f4b238
wminhyuk <seoseo1998@gmail.com>
2025-03-12 12:42:59 +0900

    0.1.1

--- src/myairflow/send_notify.py
+++ src/myairflow/send_notify.py
@@ -10,7 +10,8 @@ def send_noti(msg):
         "content": "by wminhyuk"
     }
     response = requests.post(WEBHOOK_URL, json=data)
-
+    
+    status_code = response.status_code
     if response.status_code == 204:
         print("Successfully sent message to Discord by wminhyuk")
     else:
