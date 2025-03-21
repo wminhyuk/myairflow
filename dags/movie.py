b9492b0c7d0deddcdf6905a6d1ecf4a128363a90
wminhyuk <seoseo1998@gmail.com>
2025-03-21 16:37:11 +0900

    2503.21.2

--- dags/movie.py
+++ dags/movie.py
@@ -19,12 +19,13 @@ with DAG(
     description='movie',
     schedule="10 10 * * *",
     start_date=datetime(2024, 1, 1),
-    end_date=datetime(2024, 1, 5),
+    end_date=datetime(2025, 1, 1),
     catchup=True,
     tags=['api', 'movie'],
 ) as dag:
-    REQUIREMENTS = ["git+https://github.com/wminhyuk/movie.git@2503.18.1"]
+    REQUIREMENTS = ["git+https://github.com/wminhyuk/movie.git@2503.21.1"]
     BASE_DIR = "~/data/movies/dailyboxoffice"
+    
 
     def branch_fun(ds_nodash):
         import os
@@ -39,26 +40,33 @@ with DAG(
         python_callable=branch_fun
     )
     
-    def fn_merge_data(ds_nodash):
-        print(ds_nodash)
-        # df read => ~/data/movies/dailyboxoffice/dt=20240101
-        # df1 = fill_na_with_column(df, 'multiMovieYn')
-        # df2 = fill_na_with_column(df1, 'repNationCd')
-        # df3 = df2.drop(columns=['rnum', 'rank', 'rankInten', 'salesShare'])
-        # unique_df = df3.drop_duplicates() # 25
-        # unique_df.loc[:, "rnum"] = unique_df["audiCnt"].rank(ascending=False).astype(int)
-        # unique_df.loc[:, "rank"] = unique_df["audiCnt"].rank(ascending=False).astype(int)
-        # save -> ~/data/movies/dailyboxoffice_merged/dt=20240101
+    def fn_merge_data(ds_nodash, base_path):
+        import pandas as pd
+        from movie.api.call import save_df, fill_unique_ranking
+        
+        PATH = f"{base_path}/dt={ds_nodash}"
+        load_df = pd.read_parquet(PATH)
+        df = fill_unique_ranking(load_df, ds_nodash)
+        save_path = save_df(df, base_path.replace("dailyboxoffice", "merge"), ['dt'])
+        
+        print("::group::movie df merge save...")
+        print("save_path--->" + save_path)
+        print("ds_nodash--->" + ds_nodash)
+        print("::endgroup::")
+        
         
     merge_data = PythonVirtualenvOperator(
         task_id='merge.data',
         python_callable=fn_merge_data,
         system_site_packages=False,
         requirements=REQUIREMENTS,
+        op_kwargs={
+                "base_path": BASE_DIR
+        }
     )
 
     def common_get_data(ds_nodash, url_param, base_path):
-        from movie.api.call import call_api, list2df, save_df
+        from movie.api.call import call_api, list2df, save_df, gen_url
         data = call_api(dt=ds_nodash, url_param=url_param)
         df = list2df(data, ds_nodash, url_param)
         partitions = ['dt'] + list(url_param.keys())
@@ -68,6 +76,7 @@ with DAG(
         print("save_path--->" + save_path)
         print("url_param--->" + str(url_param))
         print("ds_nodash--->" + ds_nodash)
+        print("gen_url----->" + gen_url(ds_nodash, url_param))
         print("::endgroup::")
 
     
@@ -145,13 +154,13 @@ with DAG(
     get_end = EmptyOperator(task_id='get.end')
     
     make_done = BashOperator(
-        task_id="make.done",
+        task_id='make.done',
         bash_command="""
         DONE_BASE=/home/seominhyuk/data/movies/done/dailyboxoffice
-        mkdir -p $DONE_BASE/dt={{ds_nodash}}
-        touch $DONE_BASE/dt={{ds_nodash}}/_DONE
+        mkdir -p $DONE_BASE/{{ ds_nodash }}
+        touch $DONE_BASE/{{ ds_nodash }}/_DONE
         """
-    )    
+        )
 
     start >> branch_op
 
@@ -161,4 +170,3 @@ with DAG(
     get_start >> [multi_y, multi_n, nation_k, nation_f, no_param] >> get_end
 
     get_end >> merge_data >> make_done >> end
-
--- dags/movie_after.py
+++ dags/movie_after.py
@@ -1,37 +1,107 @@
+from pprint import pprint
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import (
-        BranchPythonOperator, 
-        PythonVirtualenvOperator,
+    PythonVirtualenvOperator,
 )
 
 from airflow.sensors.filesystem import FileSensor
 
+DAG_ID = "movie_after"
+
 with DAG(
-    'movie_after',
+    DAG_ID,
     default_args={
-        'depends_on_past': True,
-        'retries': 1,
-        'retry_delay': timedelta(seconds=3),
+        "depends_on_past": True,
+        "retries": 1,
+        "retry_delay": timedelta(seconds=3),
     },
     max_active_runs=1,
     max_active_tasks=5,
-    description='movie',
+    description="movie after",
     schedule="10 10 * * *",
     start_date=datetime(2024, 1, 1),
-    end_date=datetime(2024, 1, 5),
+    end_date=datetime(2025, 1, 1),
     catchup=True,
-    tags=['api', 'movie'],
+    tags=["api", "movie", "sensor"],
 ) as dag:
+    REQUIREMENTS = ["git+https://github.com/seominhyuk/movie.git@2503.21.1"]
+    BASE_DIR = f"/home/seominhyuk/data/{DAG_ID}"
+
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
-    
 
-    
-    check_file = FileSensor(task_id="check.done",
-                            filepath="/home/seominhyuk/data/movies/done/dailyboxoffice/{{ds_nodash}}/_DONE",
-                            fs_conn_id="fs_after_movie")
-    
-    start >> check_file >> end
+    check_done = FileSensor(
+        task_id="check.done",
+        filepath="/home/seominhyuk/data/movies/done/dailyboxoffice/{{ds_nodash}}/_DONE",
+        fs_conn_id="fs_after_movie",
+        poke_interval=180,  # 3분마다 체크
+        timeout=3600,  # 1시간 후 타임아웃
+        mode="reschedule",  # 리소스를 점유하지 않고 절약하는 방식
+    )
+
+    def fn_gen_meta(ds_nodash, base_path, **kwargs):
+        import pandas as pd
+        from movie.api.after import fillna_meta, read_df_or_none, save_with_mkdir
+
+        meta_path = f"{base_path}/meta/meta.parquet"
+        previous_df = read_df_or_none(meta_path)
+
+        current_df = pd.read_parquet(f"/home/seominhyuk/data/movies/merge/dt={ds_nodash}")[
+            ["movieCd", "multiMovieYn", "repNationCd"]
+        ]
+        update_meta_df = fillna_meta(previous_df, current_df)
+        
+        save_with_mkdir(update_meta_df, meta_path)
+        print(update_meta_df)
+
+    gen_meta = PythonVirtualenvOperator(
+        task_id="gen.meta",
+        python_callable=fn_gen_meta,
+        requirements=REQUIREMENTS,
+        system_site_packages=False,
+        op_kwargs={"base_path": BASE_DIR},
+    )
+
+    def fn_gen_movie(ds_nodash, base_path, **kwargs):
+        import pandas as pd
+        from movie.api.call import save_df
+        from movie.api.after import combine_df
+
+        meta_path = f"{base_path}/meta/meta.parquet"
+        meta_df = pd.read_parquet(meta_path)
+        current_df = pd.read_parquet(f"/home/seominhyuk/data/movies/merge/dt={ds_nodash}")
+
+        final_df = combine_df(meta_df, current_df, ds_nodash)
+
+        save_df(
+            final_df,
+            f"{base_path}/dailyboxoffice",
+            ["dt", "multiMovieYn", "repNationCd"],
+        )
+        print(final_df)
+
+    gen_movie = PythonVirtualenvOperator(
+        task_id="gen.movie",
+        python_callable=fn_gen_movie,
+        requirements=REQUIREMENTS,
+        system_site_packages=False,
+        op_kwargs={"base_path": BASE_DIR},
+    )
+
+    make_done = BashOperator(
+        task_id="make.done",
+        bash_command="""
+        DONE_BASE=$BASE_DIR/done
+        echo $DONE_BASE
+        mkdir -p $DONE_BASE/{{ ds_nodash }}
+        touch $DONE_BASE/{{ ds_nodash }}/_DONE
+        """,
+        env={"BASE_DIR": BASE_DIR},
+        append_env=True,
+    )
+
+    start >> check_done >> gen_meta >> gen_movie >> make_done >> end
+
